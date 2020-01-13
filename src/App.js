3c1b9a17ba0f41bf685e206f7f10f1f8da284aea
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2020-01-13 21:29:38 +0530

    added first component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Contacts extends Component {
+  render() {
+    const people = this.props.contacts;
+
+    return <ol>
+      {
+        people.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))
+      }
+    </ol>
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Contacts contacts={[
+          {name: 'Tyler'},
+          {name: 'karen'},
+          {name: 'Richard'}
+        ]} />
+      </div>
+    );
+  }
 }
 
 export default App;
