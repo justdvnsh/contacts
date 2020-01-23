2c544d3f435cf9e3e0b30be7f364d2f50a45799c
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2020-01-23 19:31:24 +0530

    addded listContacts.js component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React, {Component} from 'react';
+import ListContacts from './Components/ListContacts';
 
-class Contacts extends Component {
-  render() {
-    const people = this.props.contacts;
-
-    return <ol>
-      {
-        people.map((person) => (
-          <li key={person.name}>{person.name}</li>
-        ))
-      }
-    </ol>
-  }
-}
+const contacts = [
+ {
+   "id": "karen",
+   "name": "Karen Isgrigg",
+   "handle": "karen_isgrigg",
+   "avatarURL": "http://localhost:5001/karen.jpg"
+ },
+ {
+   "id": "richard",
+   "name": "Richard Kalehoff",
+   "handle": "richardkalehoff",
+   "avatarURL": "http://localhost:5001/richard.jpg"
+ },
+ {
+   "id": "tyler",
+   "name": "Tyler McGinnis",
+   "handle": "tylermcginnis",
+   "avatarURL": "http://localhost:5001/tyler.jpg"
+ }
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Contacts contacts={[
-          {name: 'Tyler'},
-          {name: 'karen'},
-          {name: 'Richard'}
-        ]} />
+        <ListContacts contacts={contacts} />
       </div>
     );
   }
